1b0d78bf9feb63b86491d090adcd9162597cc5bb
ilija.mitrov1 <ilija.mitrov1@nhs.net>
2021-12-01 09:11:32 +0000

    Day1

---
+++ day01.py
@@ -0,0 +1,25 @@
+with open('input_files/input.txt', 'r') as readings:
+    depths = [int(number.strip()) for number in readings.readlines()]
+
+increases = 0
+previous = depths[0]
+for depth in depths[1:]:
+    if depth > previous:
+        increases += 1
+
+    previous = depth
+
+# Answer One
+print("Number of times the depth increases:", increases)
+
+increases = 0
+previous = sum(depths[0:3])
+for index, depth in enumerate(depths[1:]):
+    depth_window_sum = sum(depths[index: index + 3])
+    if depth_window_sum > previous:
+        increases += 1
+
+    previous = depth_window_sum
+
+# Answer Two
+print("Number of times the sliding window increases:", increases)
